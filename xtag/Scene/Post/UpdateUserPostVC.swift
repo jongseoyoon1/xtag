378bb596d75add36285141aab5806c3f8d4ee1d6
jong <yjs@brint.info>
2022-08-05 11:53:28 +0900

    no message

---
+++ xtag/Scene/Post/UpdateUserPostVC.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateUserPostVC.swift
+//  xtag
+//
+//  Created by Yoon on 2022/08/05.
+//
+
+import UIKit
+
+class UpdateUserPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
